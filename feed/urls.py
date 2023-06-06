f5a04f4af5b1f996f18c2c6c0a9545b266d91ec9
kwesy <onesimusgravessampson1@gmail.com>
2023-06-06 05:35:35 +0000

    added password reset

--- feed/urls.py
+++ feed/urls.py
@@ -20,8 +20,8 @@ urlpatterns = [
     path('documents/downloads/<int:document_id>/', view=views.downloads , name='document_downlaod'),
     path('send-email/<int:document_id>/', views.send_email, name='send_email'),
 
-    # path('password-reset/', auth_views.PasswordResetView.as_view(template_name='account/password_reset.html'), name='password_reset'),
-    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='account/password_reset_done.html'), name='password_reset_done'),
-    # path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='account/password_reset_confirm.html'), name='password_reset_confirm'),
-    # path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='account/password_reset_complete.html'), name='password_reset_complete'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='account/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='account/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='account/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='account/password_reset_complete.html'), name='password_reset_complete'),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
